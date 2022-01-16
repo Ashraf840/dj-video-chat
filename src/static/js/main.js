51280b1b7d6f0a2d421856fa1a5e5e004e5d2ec8
Ashraf840 <shift2unleashed2011@gmail.com>
2022-01-16 21:39:04 +0600

    get the userName-input to the JavaScript file

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,32 @@
-console.log('Hello!')
+//console.log('Hello!')
+
+
+
+// get all the HTML DOM element
+var label_Username = document.querySelector('#label-username');
+var input_Username = document.querySelector('#input-username');
+var btn_Join = document.querySelector('#btn-join');
+
+// store the value from the username-input-field
+var username;
+
+
+btn_Join.addEventListener('click', () => {
+    username = input_Username.value;
+
+    // if the username-input-field is empty, don't let the user to join the room.
+    if (username == '') {
+        return;
+    }
+
+    // if the username is not empty, then clear the username-input-field and disable that input-field as well.
+    input_Username.value = '';
+    input_Username.disabled = true;
+    input_Username.style.visibility = 'hidden';
+
+    // also disable the join-room btn
+    btn_Join.disabled = true;
+    btn_Join.style.visibility = 'hidden';
+
+    label_Username.innerHTML = username;
+});
